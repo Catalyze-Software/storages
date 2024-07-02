320f0ae65cc5e6ef109c1f8215a420ba38f5acc4
Semen Loktionov <loktionov.kh@gmail.com>
2024-07-02 13:25:45 +0300

    init repo

---
+++ src/shard_profile/src/lib.rs
@@ -0,0 +1,23 @@
+use ic_cdk::query;
+
+#[ic_cdk::query]
+fn greet(name: String) -> String {
+    common::greet(name)
+}
+
+// Hacky way to expose the candid interface to the outside world
+#[query(name = "__get_candid_interface_tmp_hack")]
+pub fn __export_did_tmp_() -> String {
+    use candid::export_service;
+    export_service!();
+    __export_service()
+}
+
+// Method used to save the candid interface to a file
+#[test]
+pub fn candid() {
+    catalyze_shared::candid::save_candid_file(
+        "../../candid/shard_profile.did",
+        __export_did_tmp_(),
+    );
+}
