0e95873871016e260cb13715c56288650b58abdd
Remco <remco@r3m.codes>
2024-09-05 11:43:38 +0200

    Deploy notification index

--- scripts/environment_new_deploy.sh
+++ scripts/environment_new_deploy.sh
@@ -4,17 +4,27 @@ env=$1
 # run it with bash
 dfx identity use catalyze_$env
 
-subjects=(profile group report event)
+subjects=(profile group report event notification)
 
-principal=$(dfx identity get-principal)
-echo principal: $principal
+proxy_development=puwkw-6qaaa-aaaap-ahmvq-cai
+proxy_staging=unset
+proxy_production=unset
+
 
 sh scripts/build.sh
 
 for sub in ${subjects[@]}; do
     shard_wasm=wasm/shard_$sub.wasm.gz
-    dfx canister create index_$sub --network $env --with-cycles 10000000000000
-    dfx canister install index_$sub --network $env --argument "(vec {principal \"$principal\"})"
+    # dfx canister create index_$sub --network $env --with-cycles 10000000000000
+    if [ $env == "development" ]; then
+        dfx canister install index_$sub --network $env --argument "(vec {principal \"$proxy_development\"})"
+    fi
+    if [ $env == "staging" ]; then
+        dfx canister install index_$sub --network $env --argument "(vec {principal \"$proxy_staging\"})"
+    fi
+    if [ $env == "ic" ]; then
+        dfx canister install index_$sub --network $env --argument "(vec {principal \"$proxy_production\"})"
+    fi
     dfx canister call --network $env index_$sub _dev_upload_wasm --argument-file <(echo "(blob \"$(hexdump -ve '1/1 "%.2x"' "$shard_wasm" | sed 's/../\\&/g')\")")
     dfx canister call --network $env index_$sub _dev_extend_shards '2'
 done
